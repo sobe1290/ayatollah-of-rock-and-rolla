645e3e108685e884c58cf74a723fb5460dc02c28
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-10-06 20:41:52 -0700

    adds config connection

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS nerdQuiz_db;
+
+CREATE DATABASE nerdQuiz_db;
---
+++ models/Category.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/connection');
+
+class Category extends Model {};
+
+Category.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: false,
+        underscored: true,
+        modelName: 'category',
+    },
+)
+
+module.exports = Category
