d564e5de443b832cf7126282cffc647c66fa902e
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-06 21:28:36 -0700

    adding some routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { /* Insert the Models Jacob Created then*/} = require('../models');
+
+router.get('/', withAuth, async (req, res) => {
+    try{ 
+      
+    } catch (err) {
+        res.status(500).json(err);
+    };   
+   
+  });
+
+router.get('/signup', async (req, res) => {
+    try{ 
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    };  
+    }
+)
+
+router.get('/login', (req, res) => {
+    try{ 
+      res.render('login');
+  } catch (err) {
+      res.status(500).json(err);
+  }; 
+  });
