ddd373e310683944616c19fb2a07c67a4a1ffb6f
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-10-13 09:29:38 -0700

    adds category GET requests to homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
-const {/* Any Models? */} = require('../models');
+const {User, Category, Quiz, Score } = require('../models');
 
 //This is the route for the main page
 router.get('/', async (req, res) => {
     try{ 
+        const activeUser = await User.findByPk(req.session.userID, {
+            include: [{ model: Quiz,
+                attributes:[
+                    'id',
+                    'title'
+                ]},
+                { model: Score,
+                attributes: [
+                    'score',
+                    'quiz_id'
+                ]}
+            ]
+        });
 
+        console.log(activeUser)
         /*Do we need to query and models to go with the homepage?*/
       
         res.render('homepage'); /*what are we calling the handlebars home page?*/
@@ -12,7 +26,7 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     };   
    
-  });
+});
 
 //This is the route to call for the sign up page
 router.get('/signup', async (req, res) => {
@@ -21,8 +35,60 @@ router.get('/signup', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     };  
+});
+
+// get all categories to render to page.
+router.get('/categories', async (req, res) => {
+    try {
+      const categoryData = await Category.findAll({
+        include: [{ model: Quiz,
+          attributes: ['title']
+        }],
+      }).catch((err) => {
+        res.json(err);
+      });
+
+      console.log(categoryData)
+
+      // const categories = categoryData.map((category) => category.get({ plain: true }));
+      // res.render('categories', { **Render all categories on category card**
+      //   categories,
+      //   logged_in: req.session.logged_in,
+      // });
+    } catch (err) {
+      res.status(500).json(err);
     }
-)
+});
+
+ // Get all quizzes under chosen category.
+router.get('/categories/:id', async (req, res) => {
+    try {
+      const quizData = await Quiz.findAll({
+        where: {
+          category_id: req.params.id
+        },
+        attributes: ['title'],
+        include: [{ model: Category,
+          attributes:['title']
+        }]
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  
+      console.log(quizData)
+  
+   // res.render('quizzes', { **Render list of quizzes under selected category**
+      //   categories,
+      //   logged_in: req.session.logged_in,
+      // });
+
+    }
+      catch (err) {
+        res.status(500).json(err);
+      }
+});
+
 
 //This is the route to call for the login page
 router.get('/login', (req, res) => {
@@ -31,7 +97,7 @@ router.get('/login', (req, res) => {
   } catch (err) {
       res.status(500).json(err);
   }; 
-  });
+});
 
 //This is the route to call to logout
 router.post('/logout', (req, res) => {
@@ -45,4 +111,4 @@ if (req.session.logged_in) {
 });
 
 
-  module.exports = router;
+module.exports = router;
