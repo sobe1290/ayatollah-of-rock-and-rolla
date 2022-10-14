246bc6bd479bd85c27f9553b65e4975c8d14882a
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-14 12:23:15 -0700

    adds quiz logic to calculate score

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,39 +1,44 @@
-//GET questions and answers from quizroutes.js
-
-//needed buttons and answers
 const submitBTN = document.getElementById('submitBTN');
-const selectedAnswer = document.getElementsByName("questionAnswers").checked = true;
-//trying to grab all correctAnswer id from the answers of the quiz.
-const correctAnswer = document.getElementById("correctAnswer");
-
 
 //variable to keep track of score
 const score =  0;
+const selectedAnswerArray = []
+const correctAnswerArray = []
 
-
-
-//TODO: create function to grab the correct answer/ and user answer and compare
 const checkAnswer = () => {
-    //debugging
-    console.log('submit button has been clicked');
-    console.log(selectedAnswer);
-    console.log(correctAnswer);
-    //TODO: put in if/else statement to check if selected answer = correct answer
-    if (selectedAnswer === correctAnswer) {
-        //add 1 to the score if correct
-        score= score + 10;
-        console.log(score);
-        return; // what are we returning? go to add up score?
+    
+    //This part takes all of the selected answers and puts them into an array
+    let allQuestions = document.getElementsByName("questionAnswers");
+  
+      allQuestions.forEach((answer) => {
+        if(answer.checked) {
+            selectedAnswerArray.push(answer.value);
+           
+        }
+      })
+
+    //This part takes all of the correct answers and puts them into an array
+    let correctAnswers = document.getElementsByName("correctAnswer");
+
+      correctAnswers.forEach((answer) => {
+        correctAnswerArray.push(answer.innerHTML);   
+      })
+
+     //This part compares the two arrays and makes a new array with only the correct answers 
+    setTimeout(() => {
+      const matchingAnswers = selectedAnswerArray.filter(function (obj) {
+        return correctAnswerArray.indexOf(obj) !== -1;
         
-    } else {
-        //do we want to put in a pentalty for incorrect answers?
-        return;
-    }
-    
-    
+      })
+
+      //This part looks at how many items are in the mathing array, and adds 10 to the "score" variable
+      console.log(matchingAnswers)
+      let score = matchingAnswers.length * 10
+      console.log(score)
+
+      //TODO: Take the score variable value and shoot it to the server, along wth the user ID, and Quiz number (be cautious of the order of operations. Probably needs to be all in an async/await)
+
+    }, "1000")
 }
-
-
 //on submit, grab total score, add it to Main.handlebars powerlevel and PUT it to User.power_level userRoutes.js
 submitBTN.addEventListener('click', checkAnswer);
-
