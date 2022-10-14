a7e60319525d4648f2c5d87608fb15371785925b
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-10-13 18:18:13 -0700

    adding db build scripts

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,43 @@
 //connects to SQL database specified in .env file.
 
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');
+
+async function createDB() {
+    return new Promise ( (res, rej)  => {
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        });
+        connection.query( 
+            'CREATE DATABASE IF NOT EXISTS nerdquiz_db',
+            function(err, results) {
+                if(err) {console.log(err)}
+                else console.log(results)
+            });
+        connection.end();
+    })
+}
+
+createDB();
+
+// // connects to db and created db if not exists
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+// });
+
+// connection.query( 
+//     'CREATE DATABASE IF NOT EXISTS nerdquiz_db',
+//     function(err, results) {
+//         if(err) {console.log(err)}
+//         else console.log(results)
+//     }
+// );
+// connection.end(); // ends sql connection to create db
 
 let sequelize;
 
@@ -11,14 +46,14 @@ if (process.env.JAWSDB_URL) {
 } 
 else {
     sequelize = new Sequelize (
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        });
 }
 
 module.exports = sequelize;
