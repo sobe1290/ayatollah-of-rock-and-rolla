69c31c85e591192750167c08c793ebb1890a2715
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-14 20:03:32 -0700

    re-scoping variables

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,44 +1,42 @@
 const submitBTN = document.getElementById('submitBTN');
 
-//variable to keep track of score
-const score =  0;
-const selectedAnswerArray = []
-const correctAnswerArray = []
-
 const checkAnswer = () => {
+
+  const selectedAnswerArray = []
+  const correctAnswerArray = []
     
     //This part takes all of the selected answers and puts them into an array
-    let allQuestions = document.getElementsByName("questionAnswers");
-  
-      allQuestions.forEach((answer) => {
-        if(answer.checked) {
-            selectedAnswerArray.push(answer.value);
-           
-        }
-      })
+  let allQuestions = document.getElementsByName("questionAnswers");
 
-    //This part takes all of the correct answers and puts them into an array
-    let correctAnswers = document.getElementsByName("correctAnswer");
+    allQuestions.forEach((answer) => {
+      if(answer.checked) {
+          selectedAnswerArray.push(answer.value);
+          
+      }
+    })
 
-      correctAnswers.forEach((answer) => {
-        correctAnswerArray.push(answer.innerHTML);   
-      })
+  //This part takes all of the correct answers and puts them into an array
+  let correctAnswers = document.getElementsByName("correctAnswer");
 
-     //This part compares the two arrays and makes a new array with only the correct answers 
-    setTimeout(() => {
-      const matchingAnswers = selectedAnswerArray.filter(function (obj) {
-        return correctAnswerArray.indexOf(obj) !== -1;
-        
-      })
+    correctAnswers.forEach((answer) => {
+      correctAnswerArray.push(answer.innerHTML);   
+    })
 
-      //This part looks at how many items are in the mathing array, and adds 10 to the "score" variable
-      console.log(matchingAnswers)
-      let score = matchingAnswers.length * 10
-      console.log(score)
+    //This part compares the two arrays and makes a new array with only the correct answers 
+  setTimeout(() => {
+    const matchingAnswers = selectedAnswerArray.filter(function (obj) {
+      return correctAnswerArray.indexOf(obj) !== -1;
+      
+    })
 
-      //TODO: Take the score variable value and shoot it to the server, along wth the user ID, and Quiz number (be cautious of the order of operations. Probably needs to be all in an async/await)
+    //This part looks at how many items are in the mathing array, and adds 10 to the "score" variable
+    console.log(matchingAnswers)
+    let score = matchingAnswers.length * Math.ceil(Math.random()*10) + matchingAnswers.length
+    console.log(score)
 
-    }, "1000")
+    //TODO: Take the score variable value and shoot it to the server, along wth the user ID, and Quiz number (be cautious of the order of operations. Probably needs to be all in an async/await)
+
+  }, "1000")
 }
 //on submit, grab total score, add it to Main.handlebars powerlevel and PUT it to User.power_level userRoutes.js
 submitBTN.addEventListener('click', checkAnswer);
