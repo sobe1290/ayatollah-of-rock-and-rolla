85ec7fa09dce7a657ab3a1669454e1ec175caadb
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-18 13:34:00 -0700

    adds changing crown on account

--- public/js/account.js
+++ public/js/account.js
@@ -1,7 +1,27 @@
 const ctx = document.getElementById('myChart');
 let power = document.getElementById('powerLevel').getAttribute('data-powerlevel');
-
+const avatar = document.getElementById('avatar');
 power = Number(power)
+
+
+switch (true) {
+    case (power <2000):
+        avatar.src = "../images/bronzeAvatar.PNG"
+        break;
+    case (power >= 2000 && power < 4000): 
+        avatar.src = "../images/silverAvatar.PNG"
+        break;
+    case (power >= 4000 && power < 6000):
+        avatar.src = "../images/goldAvatar.PNG"
+        break;
+    case (power >= 6000):
+        avatar.src = "../images/jewelAvatar.PNG"
+        break;
+    default:
+        avatar.src = "../images/bronzeAvatar.PNG"
+        break;
+}
+ console.log(power)
 
 const myChart = new Chart(ctx, {
     type: 'horizontalBar',
