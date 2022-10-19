bdfda0a3d580a63c8b5625681db40d871f0946ad
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-10-18 21:28:56 -0700

    adding Max

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -5,13 +5,19 @@ const userData = [
         user_name: 'Lord Humungus',
         email: 'Lord@Humungus.com',
         password: 'password',
-        power_level: 12
+        power_level: 9800
     },
     {
         user_name: 'Jedediah',
         email: 'gyro@captain.com',
         password: 'password',
-        power_level: 54
+        power_level: 4200
+    },
+    {
+        user_name: 'MaxRockatansky',
+        email: 'mad@max.com',
+        password: 'password',
+        power_level: 2600
     },
 ];
 
