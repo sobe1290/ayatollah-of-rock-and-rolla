8f7411cfe175925a9cb4e3b4014d38260cdcd537
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-10-26 10:34:13 -0700

    fixes quiz deletion when scores exist

--- devTools/makeDB.js
+++ devTools/makeDB.js
@@ -1,25 +1,25 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// require('dotenv').config();
 
-// connects to db and created db if not exists
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+// // connects to db and created db if not exists
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+// });
 
-connection.query( 
-    'DROP DATABASE IF EXISTS nerdquiz_db',
-    function(err, results) {
-        if(err) {console.log(err)}
-        else console.log(results)
-    }
-);
-connection.query( 
-    'CREATE DATABASE IF NOT EXISTS nerdquiz_db',
-    function(err, results) {
-        if(err) {console.log(err)}
-        else console.log(results)
-    }
-);
-connection.end(); // ends sql connection to create db
+// connection.query( 
+//     'DROP DATABASE IF EXISTS nerdquiz_db',
+//     function(err, results) {
+//         if(err) {console.log(err)}
+//         else console.log(results)
+//     }
+// );
+// connection.query( 
+//     'CREATE DATABASE IF NOT EXISTS nerdquiz_db',
+//     function(err, results) {
+//         if(err) {console.log(err)}
+//         else console.log(results)
+//     }
+// );
+// connection.end(); // ends sql connection to create db
